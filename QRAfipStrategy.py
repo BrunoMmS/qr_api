346edaa84c28dfdebb83b50f5bdb4a1d4c15d5ab
BrunoMmS <brunommsosa@gmail.com>
2025-07-21 12:19:30 -0300

    abstraccion para generar la imagen qr y codificarla en base64

--- QRAfipStrategy.py
+++ QRAfipStrategy.py
@@ -1,5 +1,3 @@
-import qrcode
-import io
 import base64
 import json
 from QRStrategy import QRStrategy
@@ -9,7 +7,6 @@ class QRAfipStrategy(QRStrategy):
     def generate_qr(self, data) -> dict:
         url_afip = "https://www.afip.gob.ar/fe/qr/?p="
 
-        
         if isinstance(data, dict):
             text_str = json.dumps(data, separators=(',', ':'))  
         else:
@@ -18,13 +15,4 @@ class QRAfipStrategy(QRStrategy):
         data_base64 = base64.b64encode(text_str.encode('utf-8')).decode('utf-8')
         qr_data = url_afip + data_base64
 
-        qr = qrcode.QRCode(version=1, box_size=10, border=4)
-        qr.add_data(qr_data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill_color="black", back_color="white")
-        buffered = io.BytesIO()
-        img.save(buffered, format="PNG")
-        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-
-        return {"qr_base64": img_str}
+        return super().generate_default_qr(qr_data)
--- QRStrategy.py
+++ QRStrategy.py
@@ -1,6 +1,20 @@
 from abc import ABC, abstractmethod
+import qrcode
+import io
+import base64
 
 class QRStrategy(ABC):
     @abstractmethod
     def generate_qr(self, data: str) -> dict:
         pass
+
+    def generate_default_qr(self, data: str) -> dict:
+        qr = qrcode.QRCode(version=1, box_size=10, border=4)
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffered = io.BytesIO()
+        img.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        return {"qr_base64": img_str}
